62b6641750990687942c2b233d940c6419db0ef1
Marat Shakirov <marat@minbash.de>
2025-05-10 11:56:01 +0200

    fix: add LICENSE && README && updated docs

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Graphtracks Client
 
 
+Graphtracks API definition can be found in [openapi.yaml](./openapi.yaml)
+
+API's can be used directly or with provided SDK
+
 Running [example](./example/index.ts):
 
 ```bash
@@ -14,3 +18,11 @@ will output
 npx ts-node example/index.ts did:plc:a4pqq234yw7fqbddawjo7y35
 ```
 
+
+
+> [!WARNING]
+> This project, `analytics-sdk`, is an independent, open-source software development kit designed to interact with Graphtracks API. It is not affiliated with, endorsed by, or sponsored by Bluesky PBC or any of its subsidiaries.
+>
+> "Bluesky" and related marks are trademarks of their respective owners. Use of these names is solely for descriptive purposes to indicate compatibility and does not imply any official endorsement.
+>
+> All trademarks and registered trademarks are the property of their respective owners.
--- example/index.ts
+++ example/index.ts
@@ -3,17 +3,21 @@ import { Configuration } from "../src/configuration";
 import { Metric, Network, Timeframe } from "../src/model";
 
 const did = process.argv[2];
+
+// Initialize API client
 const api = new GraphTracksAnalyticsApi(new Configuration({
     apiKey: process.env.GRAPHTRACKS_API_KEY,
 }));
 
+// Get followers stats for a given account
 api.getGlobalStatsForAccountAPI({
-    network: Network.BlueSky,
-    accountId: did,
-    metric: Metric.Followers,
-    from: '2025-05-01',
-    timeframe: Timeframe._7d,
-    bucket: (3600 * 24).toString(),
+    network: Network.BlueSky, // Only Bluesky is supported 
+    accountId: did, // atproto account did
+    metric: Metric.Followers, // Metric to query
+    from: '2025-05-01', // Start date in UTC
+    timeframe: Timeframe._7d, // relative amount of time since `from` to get metric
+    // to: '2025-05-07', // End date in UTC. Cannot be passed together with timeframe
+    bucket: (3600 * 24).toString(), // Interval between datapoints. It is recommended to keep datapoints count low both for UX and performance
 }).then((res) => {
     console.log(res.data);
 });
