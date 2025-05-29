3bc951d9a6bd350a17e36189182519918386088b
Marat Shakirov <marat@minbash.de>
2025-05-29 22:34:46 +0200

    fix: fmt

--- example/index.ts
+++ example/index.ts
@@ -19,7 +19,7 @@ api
 		metric: Metric.Followers, // Metric to query
 		from: "2025-05-01", // Start date in UTC
 		timeframe: Timeframe._7d, // relative amount of time since `from` to get metric
-		// to: '2025-05-07', // End date in UTC. Cannot be passed together with timeframe
+		// to: '2025-05-07', // End date in UTC.
 		bucket: (3600 * 24).toString(), // Interval between datapoints. It is recommended to keep datapoints count low both for UX and performance
 	})
 	.then((res) => {
